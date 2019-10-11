a261015b5331dde7451a0195398f78b8dabefefa
Roberto Minelli <rminelli@gmail.com>
2019-10-11 11:43:04 -0300

    fix home title

--- frontend/src/components/home/Home.jsx
+++ frontend/src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Main from '../template/Main'
 
 export default props =>
-    <Main icon="fa fa-home" title="Início" subtitle="Projeto CRUD - React">
+    <Main icon="fa fa-home" title="Home" subtitle="CRUD Project with React">
         <div className="display-4">Welcome!</div>
         <hr />
         <p className="mb-0">User Registration and Maintenance System - CRUD React</p>
